58d72f9c8a9e48334efeadb158274124b4e6fc17
Neelabjo Shubhashis Choudhury <neelabjo.s.choudhury@gmail.com>
2018-04-21 23:23:50 +0530

    Add files via upload

---
+++ top_words.py
@@ -0,0 +1,16 @@
+from string import punctuation
+from operator import itemgetter
+
+N = 50
+words = {}
+
+words_gen = (word.strip(punctuation).lower() for line in open("twitter_streaming_india_usa.txt") 
+                                             for word in line.split())
+                                          
+for word in words_gen:
+    words[word] = words.get(word, 0) + 1
+
+top_words = sorted(words.items(), key=itemgetter(1), reverse=True)[:N]
+
+for word, frequency in top_words:
+    print ("%s: %d" % (word, frequency))
